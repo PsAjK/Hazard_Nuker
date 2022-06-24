55759d93bc48a82f09d4b4eb2ef933ca11d2788f
PsAjK <90871967+PsAjK@users.noreply.github.com>
2022-06-24 03:22:59 +0200

    Add files via upload

---
+++ HazardNuker/setup.bat
@@ -0,0 +1,14 @@
+@echo off
+pip install requests
+pip install selenium
+pip install bs4
+pip install colorama
+pip install pycryptodome
+pip install pypiwin32
+pip install PIL-Tools
+pip install pylibcheck
+pip install pyinstaller
+pip install psutil
+pip install keyboard
+pip install httpx
+exit
